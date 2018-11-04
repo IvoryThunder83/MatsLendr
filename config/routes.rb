c5100c41f2456288a54f3c38f9529eca50cbdfa2
Mat <mat@Mats-MacBook-Pro.local>
2018-11-04 20:08:09 +1100

    added views devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'categories/index'
-  get 'categories/show'
+  get '/categories', to: 'categories#index' 
+  get '/categories/:id', to: 'categories#show'
   resources :comments
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
