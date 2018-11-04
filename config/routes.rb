3f317081221190c5526ba2eefed29f5d53a9c217
Mat <mat@Mats-MacBook-Pro.local>
2018-11-04 20:30:57 +1100

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/categories', to: 'categories#index' 
-  get '/categories/:id', to: 'categories#show'
   resources :comments
   resources :items
+  get '/categories', to: 'categories#index' 
+  get '/categories/:id', to: 'categories#show'
+  get '/transactions', to: 'transactions#index' 
+  get '/transactions/:id', to: 'transactions#show'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
